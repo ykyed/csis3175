2d32cf46284fab6e57c24bd892aecf24398704c6
Junhee <ttl7506@gmail.com>
2024-10-09 16:58:20 -0700

    info added

--- app/src/main/java/com/example/project/DetailedInfoActivity.java
+++ app/src/main/java/com/example/project/DetailedInfoActivity.java
@@ -20,5 +20,7 @@ public class DetailedInfoActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        int i = 0;
     }
 }
